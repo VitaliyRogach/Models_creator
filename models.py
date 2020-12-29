631e15a89325f8450041b200063849eb53559d04
VitaliyRogach <60302135+VitaliyRogach@users.noreply.github.com>
2020-12-29 23:33:15 +0300

    first commit

---
+++ models.py
@@ -0,0 +1,10 @@
+
+class model1(models.Model):
+    first_name = models.CharField(max_length=50)
+    first_name_2 = models.CharField(max_length=60)
+class model2(models.Model):
+    Second_name = models.CharField(max_length=10)
+    Second_name_2 = models.CharField(max_length=20)
+class model3(models.Model):
+    Third_ name = models.CharField(max_length=90)
+    Third_name_2 = models.CharField(max_length=100)
